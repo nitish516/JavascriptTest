cd3d2465a5ac29167dc035a01e863e2f55a554c5
nitish516 <ntsh.jain@gmail.com>
2015-05-20 16:15:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/javascripttest/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.javascripttest;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private static String TAG = MainActivity.class.getSimpleName();
+    String url ="http://www.pratilipi.com/api.pratilipi/pratilipi/content?pratilipiId=4843865324388352";
+    private String content;
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView)findViewById(R.id.webView);
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, response.toString());
+                        parseJson(response);
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+            }
+        });
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(jsonObjReq,
+                "jobj_req");
+    }
+
+    void parseJson(JSONObject response) {
+        try {
+            content = response.getString("pageContent");
+            Log.d(TAG,content);
+            webView.loadData(content, "text/html", null);
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.addJavascriptInterface(new WebAppInterface(this), "Android");
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
